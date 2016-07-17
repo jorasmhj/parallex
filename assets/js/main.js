b525acf2829bc21734d9f9eadf6c764cfe6052d3
jorasmhj <jorasmhj@gmail.com>
2016-07-17 19:24:02 +0545

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(window).scroll(function(){
+     var wScroll=$(this).scrollTop();
+     //console.log(wScroll);
+     $('.logo').css({
+          'transform' : 'translate(0px,'+ wScroll/2 +'%)'
+     });
+
+     $('.back-bird').css({
+          'transform' : 'translate(0px,'+ wScroll/4 +'%)'
+     });
+
+     $('.fore-bird').css({
+          'transform' : 'translate(0px,-'+ wScroll/40 +'%)'
+     });
+
+     if (wScroll > $('.clothes-pics').offset().top-$(window).height()/1.2) {
+       //console.log('hi');
+       $('.clothes-pics figure').each(function(i){
+         setTimeout(function(){
+          $('.clothes-pics figure').eq(i).addClass('is-showing');
+        },150 * (i+1));
+
+       });
+     }
+
+     if(wScroll > $('.large-window').offset().top-$(window).height()){
+       $('.large-window').css({'background-position' : 'center '+ wScroll +'px'});
+
+       var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll/5);
+       $('.window-tint').css({'opacity' : opacity});
+     }
+});
